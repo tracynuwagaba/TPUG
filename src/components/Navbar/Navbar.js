517ec45247961893f33d190ec8f34dda8529ae96
TracyNuwagaba <tnuwagaba191@gmail.com>
2021-07-29 21:50:13 +0300

    build responsive navigation mobile first with react hooks

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiMenu, FiX } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
   return (
     <nav className='navbar'>
       <Link to='/' className='navlogo' onClick={() => setOpen(false)}>
         Logo
       </Link>
+      <div onClick={handleClick} className='nav-icon'>
+        {open ? <FiX /> : <FiMenu />}
+      </div>
       <ul className={open ? 'nav-links active' : 'nav-links'}>
         <li className='nav-item'>
-          <Link to='/' className='nav-link' onClick={() => setOpen(false)}>
+          <Link to='/' className='nav-link' onClick={closeMenu}>
             Home
           </Link>
         </li>
         <li className='nav-item'>
-          <Link to='/about' className='nav-link' onClick={() => setOpen(false)}>
+          <Link to='/about' className='nav-link' onClick={closeMenu}>
             About
           </Link>
         </li>
         <li className='nav-item'>
-          <Link
-            to='/services'
-            className='nav-link'
-            onClick={() => setOpen(false)}
-          >
+          <Link to='/services' className='nav-link' onClick={closeMenu}>
             Services
           </Link>
         </li>
         <li className='nav-item'>
-          <Link
-            to='/contact'
-            className='nav-link'
-            onClick={() => setOpen(false)}
-          >
+          <Link to='/contact' className='nav-link' onClick={closeMenu}>
             Contact
           </Link>
         </li>
